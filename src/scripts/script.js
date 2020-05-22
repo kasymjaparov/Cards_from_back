f9bfc96b474cdb3794ef62b45389999f4072b925
kasymjaparov <kasymjaparov@users.noreply.github.com>
2020-05-22 19:33:16 +0600

    add loading and error of input

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,19 +1,36 @@
+  //DOM-узлы
   const block_people=document.querySelector('.block_people');
-  function compare(a, b) {
-    if (a > b) return 1;
-    if (b > a) return -1;
-    return 0;
+  const block2_input_name=document.querySelector('input[placeholder="Name"]');
+  const block2_input_surname=document.querySelector('input[placeholder="Surname"]');
+  const block2_input_email=document.querySelector('input[placeholder="Email"]');
+  const block2_textarea=document.querySelector('#message');
+  const block2_button=document.querySelector('.block3_button');
+  const block2_error=document.querySelector('.block3_error');
+  const load=document.querySelector('.lds-hourglass');
+
+//Ошибка пустых input-ов
+block2_button.addEventListener('click',function(){
+  if(
+    block2_input_surname.value.length==0||
+    block2_input_name.value.length==0||
+    block2_input_email.value.length==0){
+    block2_error.style.display='block'
+  }else{
+    block2_error.style.display='none'
   }
-  
+})
+     
+/*Обработка данных с бэка*/ 
 fetch('https://randomuser.me/api/?results=8')
         .then(response => response.json())
+        //Сортирвка объектов
         .then(data => {
 data.results.sort((a,b) => {
   return a.dob.age-b.dob.age;
 });
-
+//Создание карточек
  data.results.forEach(item => {
-   console.log(item);
+   load.style.display='none'
                 const person=document.createElement('div');
                 person.classList.add('person');
               block_people.appendChild(person);
@@ -32,5 +49,5 @@ data.results.sort((a,b) => {
              person_img.src=item.picture.large;
 })
 })
-      
-            
+
+     
